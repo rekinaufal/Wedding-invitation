4ed728e0cce396f97103b44697e67380e43b0fb4
rekinaufal <57434261+rekinaufal@users.noreply.github.com>
2022-07-03 08:46:19 +0700

    1

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/post">Post</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                @auth
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Welcome, {{ auth()->user()->name }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="/dashboard">My Dashboard</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <form action="/logout" method="post">
+                            @csrf
+                            <button type="submit" class="dropdown-item">Logout</button>
+                        </form>
+                    </ul>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link" href="/login">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/register">Registrasi</a>
+                </li>
+                @endauth
+
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ routes/web.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
+use App\Models\Category;
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        'pageTitle' => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'pageTitle' => "About"
+    ]);
+});
+
+//Route::get('/post', [PostController::class, 'index']);
+
+// middleware > defaultnya itu /home, untuk mengubahnya (app/http/providers/routeservice) ganti /home
+// berikan name('login'), karna pada (app/http/middleware/authenticate) 'login', agar membaca route get/login itu namanya login   
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);   
+Route::post('/logout', [LoginController::class, 'logout']);   
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');  
+Route::post('/register', [RegisterController::class, 'store']); 
+
+Route::get('/dashboard', function(){
+    return view ('dashboard.index');
+})->middleware('auth');  
+// jika ingin setting masuk ke register tidak harus login terlebih dahulu, pakai middleware'guest'
+// jika ingin setting tidak bisa masuk ke dashboard harus login terlebih dahulu, pakai middleware'auth'
+
+Route::get('/post', [PostController::class, 'index'])->name('post.index');
+Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
+// menamaan pada      ini  \|/ (category) harus sama dengan ini \|/  $category  
+Route::get('/categories/{category:slug}', function(Category $category){
+    return view ('/post/category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name,
+        'pageTitle' => 'Category '.$category->name
+    ]);
+});
+
+Route::get('/authors/{user}', function(User $user){
+    return view ('/post/index', [
+        'title' => 'User Post',
+        'posts' => $user->posts,
+        'pageTitle' => 'User Post'
+    ]);
+});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
